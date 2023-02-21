8412c9c22d6d55210d7ba3f6e44a6e73e1226c92
Ashish <90093173+ashurohilla@users.noreply.github.com>
2023-02-21 21:38:19 +0530

    changes about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,64 +1,28 @@
 import React from 'react'
-import img1 from "../assets/Images/1.webp";
-import img2 from "../assets/Images/2.webp";
-import img3 from "../assets/Images/3.webp";
-import img4 from "../assets/Images/4.webp";
-import img5 from "../assets/Images/5.webp";
-import img6 from "../assets/Images/6.webp";
-import img10 from "../assets/Images/10.webp";
-import img11 from "../assets/Images/11.webp";
+import gif1 from "../assets/fashion_gif/gif_1.gif";
+import gif2 from "../assets/fashion_gif/gif_2.gif";
+import gif3 from "../assets/fashion_gif/gif_3.gif";
+import gif4 from "../assets/fashion_gif/gif_4.gif";
+import gif5 from "../assets/fashion_gif/gif_5.gif";
 
 function About() {
   return (
-  <div className=" mx-auto  justify-center item-center " id='about'>
-    <h1 className=' pt-32 flex md:text-4xl sm:text-3xl ss:text-2xl text-sm  text-white justify-center item-center'>Fashion is like eating, you shouldn't stick to the same menu </h1>  
-<div className="max-w-[1320px] grid md:grid-cols-4  sm:grid-cols-3 ss:grid-cols-2 xs:grid-cols-1 grid-flow-row gap-4 mx-auto px-[20px] mt-10">
-<div className=' shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125  w-72 p-4 m-4"src={img1} alt="images">
-    </img>
-
-  </div>
-  <div className='shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125  w-72 p-4 m-4"src={img2} alt="images">
-    </img>
-
-  </div>
-  <div className=' shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img3} alt="images">
-    </img>
-
-  </div>
-  <div className=' shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img4} alt="images">
-    </img>
-
-  </div>
-  <div className='shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img5} alt="images">
-    </img>
-
-  </div>
-  <div className=' shadow-lg overflow-hidden '>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img6} alt="images">
-    </img>
-
-  </div>
-  <div className=' shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img10} alt="images">
-    </img>
-
-  </div>
-  <div className='shadow-lg overflow-hidden'>
-    <img className = "hover:scale-125 w-72 p-4 m-4"src={img11} alt="images">
-    </img>
-
-  </div>
-</div>
-  
-  </div> 
-  
-
-  
+    <div className="max-w-[1320px] mx-auto " id='product'>
+      <h1 className='text-4xl text-center py-5'>
+        Featured Products
+      </h1>
+      <div>
+        <h2 className='text-4xl  py-5'>
+        A moment of light fashion, this trend has all of the fun.
+        </h2>
+        <img  className =" h-100" src={gif1} alt="fashion">
+        </img>
+        {/* <img className =" h-100" src={gif2} alt="" />
+          <img className =" h-100" src={gif3} alt="" />
+          <img className =" h-100" src={gif4} alt="" />
+          <img className =" h-100" src={gif5} alt="" /> */}
+      </div>
+    </div>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,19 @@ import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-re
 import 'pure-react-carousel/dist/react-carousel.es.css';
 function Home() {
   return (
-    <div className=' md:mt-[300px] sm:mt-[180px] ss:mt-[100px] '  >
+    <div className='mt-[px] md:mt-[300px] sm:mt-[180px] ss:mt-[110px] '  >
      <CarouselProvider
         naturalSlideWidth={50}
         naturalSlideHeight={25}
         totalSlides={3}
       >
-        <Slider>
+     <div className=''>
+     <Slider>
           <Slide index={0}><img className='w-44' src={image1} alt="image1" /></Slide>
           <Slide index={1}><img className='w-44' src={image2} alt="image2" /></Slide>
           <Slide index={2}><img className='w-44' src={image1} alt="image3" /></Slide>
         </Slider>
+     </div>
         <ButtonBack>Back</ButtonBack>
         <ButtonNext>Next</ButtonNext>
       </CarouselProvider>
